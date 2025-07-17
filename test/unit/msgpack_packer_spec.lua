6fd56bae71c6b33db725ca66ec309e74528e1cb3
vladihnoo <vlad.pistea@bulme.at>
2025-07-17 23:55:53 +0200

    switch to ffi.cdef and init packer via packer_string_buffer()

--- test/unit/msgpack_packer_spec.lua
+++ test/unit/msgpack_packer_spec.lua
@@ -1,22 +1,39 @@
-local t = require('test.unit.testutil') 
-local ffi = t.ffi 
-local cimport = t.import 
-local lib = cimport('./src/nvim/msgpack_rpc/packer.h')
-local packer = ffi.new("PackerBuffer[1]") 
+local t = require('test.unit.testutil')
+local ffi = t.ffi
 
-describe("MessagePack fixstr encoding",function()
-  it("should verify that strings between 20 and 31 bytes are encoded as fixstr",function()
-    for len = 20,31 do 
-      local str = string.rep("a", len) 
-      local str_c = ffi.new("String",{data = t.to_cstr(str), size = len}) 
-      lib.mpack_str(str_c, packer) 
-      local packed = ffi.string(packer[0].ptr, packer[0].size) 
-      local first_byte = string.byte(packed,1) 
+ffi.cdef[[
+  typedef struct {
+    char *ptr;
+    char *endptr;
+    size_t size;
+    size_t cap;
+  } PackerBuffer;
 
-      t.eq(first_byte,0xa0+len) 
-      end
+  typedef struct {
+    const char *data;
+    size_t size;
+  } String;
 
+  void mpack_str(String str, PackerBuffer *packer);
+  PackerBuffer packer_string_buffer(void);
+]]
 
+local lib = ffi.C
+local packer = ffi.new("PackerBuffer[1]", { lib.packer_string_buffer() })
+
+describe("MessagePack fixstr encoding", function()
+  it("should verify that strings between 20 and 31 bytes are encoded as fixstr", function()
+    for msg_len = 20, 31 do
+      local str = string.rep("a", msg_len)
+      local cstr = t.to_cstr(str)
+      local str_c = ffi.new("String", { data = cstr, size = msg_len })
+      packer[0].size = 0
+      lib.mpack_str(str_c, packer)
+      local packed = ffi.string(packer[0].ptr, packer[0].size)
+      local first_byte = string.byte(packed, 1)
+      local expected = 160 + msg_len
+      t.eq(first_byte, expected)
     end
   end)
-end) 
+end)
+
